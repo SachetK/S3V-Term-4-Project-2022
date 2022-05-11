9a04cb247eaeb1f3de87fe2c22d5563cd5b1f9bd
SachetK <71898487+SachetK@users.noreply.github.com>
2022-05-10 22:16:37 -0400

    HTTP commands for Person model

--- src/main/java/com/S3V/Event/Sign/In/Tracker/service/PersonService.java
+++ src/main/java/com/S3V/Event/Sign/In/Tracker/service/PersonService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -17,11 +19,23 @@ public class PersonService {
         this.personDao = personDao;
     }
 
-    public int addPerson(Person person){
+    public int addPerson(Person person) {
         return personDao.insertPerson(person);
     }
 
     public List<Person> getAllPeople() {
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person person) {
+        return personDao.updatePersonById(id, person);
     }
 }
