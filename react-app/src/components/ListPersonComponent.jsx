3e8df7e9cfcadb9f9b1a710e5ae52e5e779f5220
SachetK <71898487+SachetK@users.noreply.github.com>
2022-05-27 19:33:45 -0400

    ListPerson

---
+++ react-app/src/components/ListPersonComponent.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class ListPersonComponent extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            people: []
+        }
+    }
+    
+    
+    render() {
+        
+        return (
+            <div>
+                <h2 className = 'text-center'> People List </h2>
+                
+                <div className = 'rows'>
+                    <table className='table table-striped table-bordered'>
+
+                        <thead>
+                            <tr>
+                                <th> Person Name </th>
+                                <th> Person MCPS ID </th>
+                                <th> Person Ticket </th>
+                                <th> Actions </th>
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.person.map(person => 
+                                    <tr key = {person.id}>
+                                        <td> {person.name}</td>
+                                        <td> {person.countyId}</td>
+                                        <td> {person.ticket}</td>
+                                    </tr>
+                                )
+                            }
+                       </tbody>
+
+                   </table>
+
+                </div> 
+            </div>
+        );
+    }
+}
+
+export default ListPersonComponent;
