849dafb6d9d82da1cdb478a99df4391d831c8b5f
SachetK <71898487+SachetK@users.noreply.github.com>
2022-05-30 08:50:54 -0400

    Add React routing with footers and headers

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListPersonComponent from './components/ListPersonComponent';
 import LoginComponent from './components/LoginComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListPersonComponent />
+    <div>
+      <Router>
+          <HeaderComponent />
+          <div className = "container">
+            <Switch>
+              <Route exact path = "/" component = {LoginComponent}></Route>
+              <Route path = "/login" component = {LoginComponent}></Route>
+              <Route path = "/students" component = {ListPersonComponent}></Route>
+              <LoginComponent />
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
