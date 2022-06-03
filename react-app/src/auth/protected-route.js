7c7a27ad297b572e11b633e7277d72c223fea883
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-03 07:51:28 -0400

    Complete Authentication redirection

--- react-app/src/auth/protected-route.js
+++ react-app/src/auth/protected-route.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
-import { Loading } from '../components/index';
 
 const ProtectedRoute = ({ component, ...args }) => (
   <Route
     component={withAuthenticationRequired(component, {
-      onRedirecting: () => <Loading />,
+      onRedirecting: () => window.location.origin,
     })}
     {...args}
   />
