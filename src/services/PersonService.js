4d942c13fb42a57cc648094d2e292b2581853b92
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-06 02:27:55 -0400

    production

--- src/services/PersonService.js
+++ src/services/PersonService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PERSON_API_BASE_URL = "http://localhost:8080/api/v1/people"
+const PERSON_API_BASE_URL = "https://event-check-in-backend.herokuapp.com/api/v1/people"
 
 class PersonService {
     
