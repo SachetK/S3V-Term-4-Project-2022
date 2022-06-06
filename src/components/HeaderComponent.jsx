307002295629c95091fe67822c8bd259ea747fc4
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-06 00:20:19 -0400

    Move backend to diff repo

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import AuthenticationButton from './AuthenticationButton';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark justify-content-between">
+                      <div><a href='http://localhost:3000' className='navbar-brand'>Poolesville Event Check In Tracker</a></div>
+                      <div className='navbar-nav'>
+                        <div><a href='http://localhost:3000/students' className="nav-item nav-link">Students List</a></div>
+                        <div><a href='http://localhost:3000/help' className="nav-item nav-link">Help</a></div>
+                        <div className="nav-item"><AuthenticationButton /></div>
+                      </div>
+                    </nav>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
