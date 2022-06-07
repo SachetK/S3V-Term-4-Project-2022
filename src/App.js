6db3949a3984e4df1d47da53b2e81d940a66b3d7
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-07 07:37:40 -0400

    Change ListPerson to Functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ListPersonComponent from './components/ListPersonComponent';
 import HomeComponent from './components/HomeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import HelpComponent from './components/HelpComponent';
 import { Container } from 'react-bootstrap';
 import ProtectedRoute from './auth/protected-route';
 import useRedirectToHTTPS from './auth/useRedirectToHTTPS';
+import ListPersonComponentCopy from './components/ListPersonComponentCopy';
 
 function App() {
   useRedirectToHTTPS();
@@ -18,7 +18,7 @@ function App() {
           <Switch>
             <Route exact path = "/" component = {HomeComponent}></Route>
             <Route path = "/login" component = {HomeComponent}></Route>
-            <ProtectedRoute path = "/students" component = {ListPersonComponent}></ProtectedRoute>
+            <ProtectedRoute path = "/students" component = {ListPersonComponentCopy}></ProtectedRoute>
             <Route path = "/help" component = {HelpComponent}></Route>
             <HomeComponent />
           </Switch>
---
+++ src/components/ListPersonComponentCopy.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import PersonService from '../services/PersonService';
+import '../App.css';
+
+import { Table, Button, ButtonGroup } from "react-bootstrap";
+
+const ListPersonComponentCopy = () => {
+    const [people, setPeople] = useState([]);
+    const [searchTerm, setTerm] = useState('');
+
+    
+    useEffect(() => {
+        PersonService.getPeople().then(
+            (res) => {
+                setPeople(res.data);
+            }
+            )
+        }, []);
+    
+    return (
+        <div>
+            <h2 className = "text-center" style={{ color: "white"}}> Student List </h2>
+            <input type="text" value = {searchTerm} onChange = {(e) => setTerm(e)}></input>
+                <Table striped bordered hover variant = "dark">
+                    <thead>
+                        <tr>
+                            <th> Student Ticket Number</th>
+                            <th> MCPS ID</th>
+                            <th> Student Last Name</th>
+                            <th> Student Middle Initial</th>
+                            <th> Student First Name</th>
+                            <th> Grade</th>
+                            <th> Payment Method</th>
+                            <th> Guest Y/N</th>
+                            <th> Guest Ticket Number</th>
+                            <th> Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            people.map(
+                                person =>
+                                <tr key = {person.id}>
+                                    <td> {person.ticket} </td>
+                                    <td> {person.countyId} </td>
+                                    <td> {person.lastName} </td>
+                                    <td> {person.middleInitial} </td>
+                                    <td> {person.firstName} </td>
+                                    <td> {person.grade} </td>
+                                    <td> {person.paymentMethod} </td>
+                                    <td> {person.guest} </td>
+                                    <td> {person.guestTicket} </td>
+                                    <td> 
+                                        <ButtonGroup role = "group" size = 'sm' >
+                                            <Button variant = "dark">More Info</Button>
+                                            <Button variant = "dark">Check in</Button>
+                                        </ButtonGroup>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+        </div>
+    );
+};
+
+export default ListPersonComponentCopy;
