834242d980841779d33660ce49f9c29448e022c1
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-07 23:01:33 -0400

    Finish modal

--- src/components/ListPersonComponent.jsx
+++ src/components/ListPersonComponent.jsx
@@ -8,17 +8,19 @@ const ListPersonComponent = () => {
     const [people, setPeople] = useState([]);
     // const [searchTerm, setTerm] = useState('');
     const [modalData, setModalData] = useState({});
-    const [guestData, setGuestData] = useState({});
+    const [counterpartData, setCounterpartData] = useState({});
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     
     const handleShow = (person) => {
         setModalData(person);
-        if(person.guestData == 'Y'){
-            setGuestData(people.find(o => o.ticket == person.guestTicket));
-            console.log(guestData);
+        if(person.guest === 'Y'){
+            setCounterpartData(people.find(o => o.ticket === person.guestTicket));
+        } else if (person.countyId == null) {
+            setCounterpartData(people.find(o => o.guestTicket === person.ticket));
         }
+
         setShow(true);        
     };
     
@@ -81,11 +83,11 @@ const ListPersonComponent = () => {
                 onHide={handleClose}
             >
             <Modal.Header>
-                <Modal.Title>More info on {modalData.firstName + " " + modalData.lastName}</Modal.Title>
+                <Modal.Title>More Info on {modalData.firstName + " " + modalData.lastName}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h1>Student: </h1>
-                    <table class="table">
+                    <h3>{ modalData.countyId != null ? "Student: " : "Guest: " }</h3>
+                    <table className="table">
                         <thead>
                             <tr>
                                 <th>Ticket</th>
@@ -101,22 +103,22 @@ const ListPersonComponent = () => {
                         </thead>
                         <tbody>
                             <tr>
-                                <td>{modalData.ticket} </td>
+                                <td> {modalData.ticket} </td>
                                 <td> {modalData.countyId} </td>
                                 <td> {modalData.lastName} </td>
                                 <td> {modalData.middleInitial} </td>
                                 <td> {modalData.firstName} </td>
                                 <td> {modalData.grade} </td>
-                                <td> {modalData.tdaymentMethod} </td>
+                                <td> {modalData.paymentMethod} </td>
                                 <td> {modalData.guest} </td>
                                 <td> {modalData.guestTicket} </td>
                             </tr>
                         </tbody>
                     </table>
-                   { modalData.guest === 'Y' &&
+                   { (modalData.guest === 'Y' || modalData.countyId == null) &&
                     <>
-                        <h1>Guest: </h1>
-                        <table class="table">
+                        <h3>{ modalData.countyId != null ? "Guest: " : "Student: " } </h3>
+                        <table className="table">
                             <thead>
                                 <tr>
                                     <th>Ticket</th>
@@ -126,22 +128,27 @@ const ListPersonComponent = () => {
                                     <th>FIRST</th>
                                     <th>GR</th>
                                     <th>Payment Method</th>
+                                    <th>Guest YN</th>
+                                    <th>Guest Ticket Number</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td> { guestData.ticket } </td>
-                                    <td> { guestData.countyId } </td>
-                                    <td> { guestData.lastName } </td>
-                                    <td> { guestData.middleInitial } </td>
-                                    <td> { guestData.firstName } </td>
-                                    <td> { guestData.grade } </td>
-                                    <td> { guestData.paymentMethod } </td>
+                                <td> {counterpartData.ticket} </td>
+                                <td> {counterpartData.countyId} </td>
+                                <td> {counterpartData.lastName} </td>
+                                <td> {counterpartData.middleInitial} </td>
+                                <td> {counterpartData.firstName} </td>
+                                <td> {counterpartData.grade} </td>
+                                <td> {counterpartData.paymentMethod} </td>
+                                <td> {counterpartData.guest} </td>
+                                <td> {counterpartData.guestTicket} </td>
                                 </tr>
                             </tbody>
                         </table>
                     </>
                    }
+                    <h3><a href='\help'>Logs</a></h3>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
