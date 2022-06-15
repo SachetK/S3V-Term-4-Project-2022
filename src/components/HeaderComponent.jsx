96e9b12d60e381fde662515f5b6c4a6f104f319c
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-14 21:28:48 -0400

    Final for now

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -10,6 +10,7 @@ class HeaderComponent extends Component {
                       <div><a href='/login' className='navbar-brand'>Poolesville Event Check In Tracker</a></div>
                       <div className='navbar-nav'>
                         <div><a href='/students' className="nav-item nav-link">Students List</a></div>
+                        <div><a href='/logs' className="nav-item nav-link">Logs Record</a></div>
                         <div><a href='/help' className="nav-item nav-link">Help</a></div>
                         <div className="nav-item"><AuthenticationButton /></div>
                       </div>
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Container, Row, Col, Button} from 'react-bootstrap';
 import PersonService from '../services/PersonService';
 
 const HomeComponent = () => {    
@@ -8,26 +9,37 @@ const HomeComponent = () => {
 
     return (
         <div className="container  animated fadeIn">
-            <div className="title float-left">
-                <h1 className="brand" ><a href="/index" style={{ color: "white"}}>Poolesville Event Check-In</a></h1>
-            </div>
-            <div className="help-icon float-right">
-                <a href="/help" style={{ color: "#FDD336"}}><i className="fa fa-question-circle fa-3x" /></a>
-            </div>
-            <div className="clearfix">
-
-            </div>
-            <div className="float-right">
-                
-            </div>
-            <div className="wrapper">
-                <div className="company-info">
-                    <div id="logo-wrapper">
-                        <img src="/images/PHSLogo.png" width="200" height="200" alt="Logo" id="logo"/>
-                    </div>
-                    
-                </div>
-            </div>
+            <Container fluid>
+                <Row style={{ paddingBottom: '15px', paddingLeft: '300px'}}>
+                    <Col md={11}><h1 className="brand"><a href="/index" style={{ color: "white"}}>Poolesville Event Check-In</a></h1></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="wrapper">
+                            <div className="company-info">
+                                <div id="logo-wrapper">
+                                    <img src="/images/PHSLogo.png" width="200" height="200" alt="Logo" id="logo" />
+                                </div> 
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Button href = "/students" variant="dark" style={{ height: '200px' , width: '15rem'}}>
+                            <h3 style={{ paddingTop: '70px'}}>Student List</h3>
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button href = "/logs" variant="dark" style={{ height: '200px' , width: '15rem'}}>
+                            <h3 style={{ paddingTop: '70px'}}>Log Records</h3>
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button href = "/help" variant="dark" style={{ height: '200px' , width: '15rem', backgroundColor: '#be965c'}}>
+                            <h3 style={{ paddingTop: '70px'}}>Help</h3>
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
